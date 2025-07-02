dfaeb538d1795d969096cb217f54e6d4fbd27c9f
김병민 <gimbyeongmin@byeong.local>
2025-07-03 05:07:52 +0900

    feat:uvx용 기능 수정

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import asyncio
 from mcp_server import mcp
 
+def main():
+    asyncio.run(mcp.run(transport='stdio'))
 
 if __name__ == "__main__":
-    asyncio.run(mcp.run(transport='stdio'))
+    main()
     
